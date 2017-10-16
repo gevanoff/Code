07d01921850c04539891e0f9f2e86198c507a233
Gabriel Evanoff <gevanoff@gevanoff-m01.ad.savagebeast.com>
2017-10-16 09:38:43 -0700

    alphabetical now takes CSV lines and outputs sorted CSV lines

--- python/alphabetical.py
+++ python/alphabetical.py
@@ -1,11 +1,20 @@
 #!/usr/local/bin/python3
-#not working
+#gevanoff@gmail.com
 import sys
+
+def csv_to_list(text):
+  word_list = text.replace(',',' ').replace('\\',' ').split()
+  return word_list
 
 if len(sys.argv) < 2:
   print("Please add a list to sort")
   exit(1)
 
-items = sys.argv[1].replace(',',' ').split()
+items = []
+for set in sys.argv[1:]:
+  for item in csv_to_list(set):
+    items.append(item)
 
-print(items)
+items.sort()
+
+print(", ".join(items))
